f06327c9bb3a8070e6fef75d6ebb052271f54c59
Reily Siegel <mail@reilysiegel.com>
2023-04-13 19:13:39 -0400

    Return data from database.

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
+from database import Data
 from flask import Flask, Response
 from flask_restful import Resource, Api, reqparse
+import json
 import signal
 
 app = Flask(__name__)
@@ -7,14 +9,8 @@ api = Api(app)
 
 class ArduinoData(Resource):
     def get(self):
-        # TODO: Implement actual calculations.
-        data = {
-            'high': 1,
-            'moderate': 0,
-            'low': 0
-        }
-        data['total'] = data['high'] + data ['moderate'] + data ['low']
-        return data
+        return [d.__dict__ for d in Data.get_all()]
+
 # Return 200 with no data.
 class HealthCheck (Resource):
     def get(self):
