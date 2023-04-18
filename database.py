21b089e32c8d0877cce642b095a8ec0e181eea6b
Reily Siegel <mail@reilysiegel.com>
2023-04-18 14:57:07 -0400

    Calculate counts of concussion risk.

--- database.py
+++ database.py
@@ -5,10 +5,24 @@ import sqlite3
 
 DATABASE_PATH = 'database.db'
 
+TABLE_INIT = """
+CREATE TABLE arduino_data (
+id INTEGER PRIMARY KEY,
+FSensor1 FLOAT,
+FSensor2 FLOAT,
+FSensor3 FLOAT,
+FSensor4 FLOAT,
+FSensor5 FLOAT,
+AccelX FLOAT,
+AccelY FLOAT,
+AccelZ FLOAT,
+timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);"""
+
 class Data:
     # From scalars
-    def __init__(self, F1=0.0, F2=0.0, F3=0.0, F4=0.0, F5=0.0, AX=0.0, AY=0.0,
-                 AZ=0.0, timestamp=datetime.datetime.now()):
+    def __init__(self, id=None, F1=0.0, F2=0.0, F3=0.0, F4=0.0, F5=0.0, AX=0.0,
+                 AY=0.0, AZ=0.0, timestamp=datetime.datetime.now()):
         self.F1 = F1
         self.F2 = F2
         self.F3 = F3
@@ -22,10 +36,14 @@ class Data:
         self.acceleration = math.sqrt(math.pow(AX, 2) +
                                       math.pow (AY, 2) +
                                       math.pow(AZ, 2))/0.1458
+        self.high_risk = self.acceleration >= 6814.81
+        self.med_risk = not self.high_risk and self.acceleration >= 6265.5
+        self.low_risk = not self.med_risk and not self.high_risk and self.acceleration >= 30 #5572.35
 
     @staticmethod
     def from_json(self, json):
-        return Data(json['FSensor1'],
+        return Data(None,
+                    json['FSensor1'],
                     json['FSensor2'],
                     json['FSensor3'],
                     json['FSensor4'],
@@ -35,44 +53,33 @@ class Data:
                     json['AccelZ'])
 
     def write(self):
-        q="INSERT INTO arduino_data VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)"
-        data = (self.F1, self.F2, self.F3, self.F4, self.F5, self.AX, self.AY,
-                self.AZ, self.timestamp)
-        with sqlite3.connect(DATABASE_PATH) as conn:
-            c = conn.cursor()
-            c.execute(q, data)
-            c.commit()
+        # Only write datum indicating risk
+        if (self.low_risk or self.med_risk or self.high_risk):
+            q="INSERT INTO arduino_data VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)"
+            data = (self.F1, self.F2, self.F3, self.F4, self.F5, self.AX, self.AY,
+                    self.AZ, self.timestamp)
+            with sqlite3.connect(DATABASE_PATH) as conn:
+                c = conn.cursor()
+                c.execute(q, data)
+                c.commit()
 
     @staticmethod
     def get_all():
         q="""
-SELECT FSensor1, FSensor2, FSensor3, FSensor4, FSensor5,
-        AccelX, AccelY, AccelZ, timestamp from arduino_data
+        SELECT id, FSensor1, FSensor2, FSensor3, FSensor4, FSensor5, AccelX, AccelY,
+        AccelZ, timestamp from arduino_data
         """
         data=None
         with sqlite3.connect(DATABASE_PATH) as conn:
             c=conn.cursor()
             c.execute(q)
             data=c.fetchall()
-            return [Data(d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7],d[8]) for d in data]
+            return [Data(d[0],d[1],d[2],d[3],d[4],d[5],d[6],d[7],d[8],d[9])
+                    for d in data]
 
 def init_db():
-    create_table_query = """
-    CREATE TABLE arduino_data (
-id INTEGER PRIMARY KEY,
-FSensor1 FLOAT,
-FSensor2 FLOAT,
-FSensor3 FLOAT,
-FSensor4 FLOAT,
-FSensor5 FLOAT,
-AccelX FLOAT,
-AccelY FLOAT,
-AccelZ FLOAT,
-timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
-    );"""
-
     if not os.path.isfile(DATABASE_PATH):
         with sqlite3.connect(DATABASE_PATH) as conn:
             c = conn.cursor()
-            c.execute(create_table_query)
+            c.execute(TABLE_INIT)
             print("database.db not found, file initialized")
--- server.py
+++ server.py
@@ -11,10 +11,22 @@ class ArduinoData(Resource):
     def get(self):
         return [d.__dict__ for d in Data.get_all()]
 
+class Concussions(Resource):
+    def get(self):
+        data = Data.get_all()
+        concussions = {
+            "high": sum ([1 for d in data if d.high_risk]),
+            "med": sum ([1 for d in data if d.med_risk]),
+            "low": sum ([1 for d in data if d.low_risk])
+        }
+        concussions["total"] = sum (concussions.values())
+        return concussions
+
 # Return 200 with no data.
 class HealthCheck (Resource):
     def get(self):
         return Response(status=200)
 
 api.add_resource(ArduinoData, "/data")
+api.add_resource(Concussions, "/concussions")
 api.add_resource(HealthCheck, "/ping")
